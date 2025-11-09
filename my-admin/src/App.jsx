3cb4a1b3c39f8d7cdb88941c2215a21af6662bf3
Langelihle6 <141252621+Langelihle6@users.noreply.github.com>
2025-11-09 08:52:47 +0000

    created a new codespace, added all my dependencies  and files

---
+++ my-admin/src/App.jsx
@@ -0,0 +1,20 @@
+import { Admin, Resource, ListGuesser} from "react-admin";
+/*import { Route } from "react-router-dom"; */
+import { createTrailbaseProvider } from "./dataProvider";
+
+
+const TRAILBASE_URL = "http://stunning-sniffle-g4q7x4x799g9hw9x4-4000.app.github.dev/";
+const { dataProvider} = await createTrailbaseProvider(
+  TRAILBASE_URL,
+);
+
+function App() {
+  return (
+    <Admin dataProvider={dataProvider}  >
+      <Resource name = "assignments" list= {ListGuesser} />
+      
+    </Admin>
+  );
+}
+
+export default App;
